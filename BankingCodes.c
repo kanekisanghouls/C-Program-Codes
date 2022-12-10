b0ef6c5c11c6d306775c053916a71af5227f11df
KanekiSanGhouls <101504818+kanekisanghouls@users.noreply.github.com>
2022-12-10 15:52:46 +0800

    Update BankingCodes.c

--- BankingCodes.c
+++ BankingCodes.c
@@ -1,1 +1,112 @@
-abcd
+//================================= BANKING CODES #1 =================================
+#include <stdio.h>
+
+struct bank{
+	int account_no;
+	char name[50];
+	float balance;
+};
+
+int main(){
+	struct bank b1;
+
+	printf("Enter Account Number: ");
+	scanf("%d", &b1.account_no);
+	printf("Enter Name: ");
+	scanf("%s", b1.name);
+	printf("Enter Balance: ");
+	scanf("%f", &b1.balance);
+
+	printf("\nAccount Number: %d\n", b1.account_no);
+	printf("Name: %s\n", b1.name);
+	printf("Balance: %f\n", b1.balance);
+
+	return 0;
+}
+
+//================================= BANKING CODES #2 =================================
+
+
+//================================= BANKING CODES #3 =================================
+
+
+//================================= BANKING CODES #4 =================================
+
+
+
+
+//================================= BANKING CODES #5 =================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
