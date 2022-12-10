1b754e6988e1175d7d254860648a2dea2c6c6e0a
KanekiSanGhouls <101504818+kanekisanghouls@users.noreply.github.com>
2022-12-10 15:46:52 +0800

    Update and rename 3-PatternCodes.c to PatternCodes.c

--- Patterns Codes/PatternCodes.c
+++ Patterns Codes/PatternCodes.c
@@ -185,8 +185,29 @@ int main()
 
 }
 
-
-
+//========================== PATTERNS STAR ==========================
+#include <stdio.h>
+void starPattern(int row,int col){
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=col;j++){
+            if(j>i){
+                printf(" ");
+            }
+            else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+int main(){
+    int row,col;
+    printf("Enter the value of row & col:");
+    scanf("%d%d",&row,&col);
+    starPattern(row,col);
+    return 0;
+}
 
 
 
