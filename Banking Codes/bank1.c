7059c4833a3a50ffb9e71f2f1ddb10829fd7eae6
KanekiSanGhouls <101504818+kanekisanghouls@users.noreply.github.com>
2023-09-10 17:06:08 +0800

    Create bank1.c

---
+++ Banking Codes/bank1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct bank{
+	int account_no;
+	char name[50];
+	float balance;
+};
+
+int main(){
+	struct bank b1;
+
+	printf("Enter Account Number: ");
+	scanf("%d", &b1.account_no);
+	printf("Enter Name: ");
+	scanf("%s", b1.name);
+	printf("Enter Balance: ");
+	scanf("%f", &b1.balance);
+
+	printf("\nAccount Number: %d\n", b1.account_no);
+	printf("Name: %s\n", b1.name);
+	printf("Balance: %f\n", b1.balance);
+
+	return 0;
+}
